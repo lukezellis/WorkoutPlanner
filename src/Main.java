aebbdc859051d3c39cfe33231016fc86499b3af5
lukezellis <lukezellis@gmail.com>
2025-07-30 15:06:23 -0400

    Goals added

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,27 @@ public class Main {
 
     }
 }
+/*
+ - Last workout reps
+ - Goal reps
+ - Input for reps achieved
+ - Add/remove exercise
+    - add/remove set
+ - Input for warmup
+    - show warmup rep target
+ - Input for weight per exercise
+    - raise/lower weight input
+
+
+ - Full log of all workouts
+    - categorized by exercise
+    - categorized by workout day (push/pull/legs)
+
+
+ - Timer
+    - total workout timer
+    - set timer
+    - rest timer
+
+ - Mesocycle
+*/
