24e6cb4b7d1cf8a683ad4d28c1267ac812550af0
lukezellis <lukezellis@gmail.com>
2025-07-30 20:52:35 -0400

    feature list update

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,44 @@ public class Main {
     }
 }
 /*
- - Last workout reps
- - Goal reps
- - Input for reps achieved
+ - Build week
+    - maybe start with just Day#1-3 and repeat
+ - Cycle through 3-day split (or any # of days-split)
+ - Rest day button
+    - or just input "Rest" for exercise and logs whole day complete
+        - active rest (wedding, walk, etc.) vs true rest days
+ - Last workout weight & reps
+ - Goal weight & reps
+    - Input for reps achieved
  - Add/remove exercise
     - add/remove set
- - Input for warmup
-    - show warmup rep target
+ - Skip exercise for current workout only
+    - or cut exercise early
+        - maybe just "-" option for any set
+            - count as 0 for log
+    - mention it was skipped/cut early on next workout
+        -shortened for 2+ workouts, suggest change
  - Input for weight per exercise
+    - default to previous weight from same exercise
     - raise/lower weight input
 
+ - Exercise ques/notes for each exercise
+
+ - Warmup
+    - show warmup sets/reps target
+    - input to override warmup defaults
+
+ - Rearrange exercises mid-workout
+ - Create new exercise in app
+    - Instantly add new exercise (or store for different day)
+
+ - Build workouts, while not in a workout
 
  - Full log of all workouts
+    - have dates & times
     - categorized by exercise
     - categorized by workout day (push/pull/legs)
-
+    - chart progression
 
  - Timer
     - total workout timer
@@ -26,4 +49,5 @@ public class Main {
     - rest timer
 
  - Mesocycle
+
 */
